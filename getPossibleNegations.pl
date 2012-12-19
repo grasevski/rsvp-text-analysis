2035d2db7c9519d6c29c66229fd0cdfdd03b8c58
Nicholas Grasevski <nicholas@localhost.localdomain>
2012-12-19 16:43:24 +1100

    Changed removeNegations to match only from last punctuation

--- getPossibleNegations.pl
+++ getPossibleNegations.pl
@@ -40,4 +40,5 @@ for (open my $fh, '<', $ARGV[0]; <STDIN>; print "$out\n") {
 	my ($k, $v) = split ':', $_, 2;
 	++$i while $i < $k and $user = <$fh>;
 	($out, $user) = split "\Q$v\E", $user, 2;
+	if ($out =~ /[^\w\s]([\w\s]*)$/) {$out = $1;}
 }
